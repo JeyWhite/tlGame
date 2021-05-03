3682543f2ba1aae15025c4e85cf44f2b8f1f71d3
Женя Белый <awolman93@outlook.com>
2021-05-04 02:27:38 +0700

    program separated

--- main.py
+++ main.py
@@ -103,25 +103,6 @@ def move(level, direction, pos, r: int):
     #     return level
 
 
-def game_render(level):
-    symbols = {
-        10: "█",
-        0: " ",
-        1: "X",
-        2: "B",
-        3: "×",
-        -1: "Y",
-        5: "D"
-    }
-
-    for i in symbols.keys():
-        level[level == i] = ord(symbols[i])
-    for i in range(level.shape[0]):
-        for j in range(level.shape[1]):
-            print(chr(level[i, j]), end="")
-        print("")
-
-
 def get_button():
     buttons = {
         72: UP,
